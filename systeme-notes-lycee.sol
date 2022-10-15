71e49d52e0fd5675371980528473054e8c318231
anicaise95 <112395282+anicaise95@users.noreply.github.com>
2022-10-15 10:44:09 +0200

    version 0.1

--- systeme-notes-lycee.sol
+++ systeme-notes-lycee.sol
@@ -6,13 +6,20 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SytemeVoteEleves is Ownable {
 
-    enum Matiere{ MATHS, FRANCAIS, PHYSIQUE, ANGLAIS}
-    // Une adresse un professeur
+    enum Matiere { MATHS, FRANCAIS, PHYSIQUE, ANGLAIS}
+
+    // Une adresse => un professeur
     mapping(address => Matiere) professeurs;
+
+    // Une classe => une liste d'étudiants
+    mapping(string => Student[]) classes;
+    // La liste des étudiants
+    Student[] students;
     
     struct Student {  
-        string lastname;
-        string firstname;
+        address adresse;
+        string nom;
+        string prenom;
         Matieres[] matieres;
         uint moyenneGenerale;
     }
@@ -23,8 +30,6 @@ contract SytemeVoteEleves is Ownable {
         uint moyenne; 
     }
 
-    mapping(address => Student) public students;
-   
     Matiere matieres;
 
     mapping(Matiere => uint) moyennesParMatiere;
@@ -37,13 +42,30 @@ contract SytemeVoteEleves is Ownable {
         professeurs[address(0x617F2E2fD72FD9D5503197092aC168c91465E7f2)] = Matiere.ANGLAIS;
     }
 
-    function addStudent(address _adrStudent, string memory _firstname, string memory _lastname) public onlyOwner {
-        students[_adrStudent].firstname = _firstname;
-        students[_adrStudent].lastname = _lastname;
-        students[_adrStudent].matieres.push(Matieres(Matiere.MATHS, new uint[](0), 0));
-        students[_adrStudent].matieres.push(Matieres(Matiere.FRANCAIS, new uint[](0), 0));
-        students[_adrStudent].matieres.push(Matieres(Matiere.PHYSIQUE, new uint[](0), 0));
-        students[_adrStudent].matieres.push(Matieres(Matiere.ANGLAIS, new uint[](0), 0));
+    // Retourne l'index de l'éleve dans la classe
+    function getEtudiantParNom(string memory _nom, string memory _classe) public view returns (uint){
+        for (uint i = 0; i < classes[_classe].length; i++) {
+            if ( keccak256( abi.encodePacked(classes[_classe][i].nom) ) == keccak256( abi.encodePacked(_nom) ) ){
+                return i;
+            }
+        } 
+    }
+
+    function ajouterEtudiant(address _adrStudent, string memory _nom, string memory _prenom, string memory _classe) public onlyOwner {
+        Student memory student; //= Student(_adrStudent, _nom, _prenom, new Matiere[], 0);
+        student.adresse = _adrStudent;
+        student.prenom = _prenom;
+        student.nom = _nom;
+        students.push(student);
+    
+        // MAJ du storage
+        uint index = getEtudiantParNom(_nom, _classe); // ou index = classes[_classe].length;
+        students[index].matieres.push(Matieres(Matiere.MATHS, new uint[](0), 0));
+        students[index].matieres.push(Matieres(Matiere.FRANCAIS, new uint[](0), 0));
+        students[index].matieres.push(Matieres(Matiere.PHYSIQUE, new uint[](0), 0));
+        students[index].matieres.push(Matieres(Matiere.ANGLAIS, new uint[](0), 0));
+
+        classes[_classe] = students;
     }
 
     function isProfesseur(Matiere matiereProf) private pure returns (bool output){
@@ -54,59 +76,64 @@ contract SytemeVoteEleves is Ownable {
     }
 
     // Le professeur ajoute la note de l'élève
-    function addNote(address _adrStudent, uint _note) public {
+    function addNote(string memory _nom, string memory _classe, uint _note) public {
         // On récupère la matière du porof en fonction de son adresse
         Matiere matiereNoteeByProf = professeurs[msg.sender];
         // Sil est bien reconnu comme professeur
         require(isProfesseur(matiereNoteeByProf), unicode"Seuls les profs sont autorisés");
 
+        uint index = getEtudiantParNom(_nom, _classe);
+
         // On boucle sur chaque matière       
-        for (uint i = 0; i < students[_adrStudent].matieres.length; i++){
+        for (uint i = 0; i < students[index].matieres.length; i++){
             // On chercher la matière correspondant au professeur qui renseige la note
-            if(students[_adrStudent].matieres[i].matiere == matiereNoteeByProf){
+            if(students[index].matieres[i].matiere == matiereNoteeByProf){
                 // Le prof enregistre la note de l'étudiant 
-                students[_adrStudent].matieres[i].notes.push(_note);
+                students[index].matieres[i].notes.push(_note);
             }
         }
     }
 
-    function calculerMoyenneEtudiantParMatiere(address _adrStudent) public {
+    function calculerMoyenneEtudiantParMatiere(string memory _nom, string memory _classe) public {
+
+        uint index = getEtudiantParNom(_nom, _classe);
 
         // Pour chaque matiere de l'éleve
-        for (uint i = 0; i < students[_adrStudent].matieres.length; i++){
+        for (uint i = 0; i < students[index].matieres.length; i++){
 
             uint sommeNotes = 0;
             uint nbNotes = 0;
 
             // on calcule la moyenne des notes
-            for (uint j = 0; j < students[_adrStudent].matieres[i].notes.length; i++){
-                uint note = students[_adrStudent].matieres[i].notes[j];
+            for (uint j = 0; j < students[index].matieres[i].notes.length; i++){
+                uint note = students[index].matieres[i].notes[j];
                 sommeNotes += note;
                 nbNotes++;
             }
 
-            students[_adrStudent].matieres[i].moyenne = sommeNotes * 100 / nbNotes;           
+            students[index].matieres[i].moyenne = sommeNotes * 100 / nbNotes;           
         }
     }
 
-    function calculerMoyenneClasseParMatiere() public {
+    function calculerMoyenneClasseParMatiere(string memory _nom, string memory _classe) public {
 
     }
 
-    function calculerMoyenneGeneraleEtudiant(address _adrStudent) public {
+    function calculerMoyenneGeneraleEtudiant(string memory _nom, string memory _classe) public {
 
-         uint sommeMoyennesParMatiere = 0;
-         uint nbMatieres = students[_adrStudent].matieres.length;
-         uint moyenneGenerale = 0;
+        uint index = getEtudiantParNom(_nom, _classe);
+        uint sommeMoyennesParMatiere = 0;
+        uint nbMatieres = students[index].matieres.length;
+        uint moyenneGenerale = 0;
 
         // Pour chaque matiere de l'éleve
-        for (uint i = 0; i < students[_adrStudent].matieres.length; i++){
+        for (uint i = 0; i < students[index].matieres.length; i++){
             // Sommes des moyennes (préalablement calculées via calculerMoyenneEtudiantParMatiere)
-            sommeMoyennesParMatiere += students[_adrStudent].matieres[i].moyenne;         
+            sommeMoyennesParMatiere += students[index].matieres[i].moyenne;         
         }
 
         moyenneGenerale = sommeMoyennesParMatiere * 100 / nbMatieres;
-        students[_adrStudent].moyenneGenerale = moyenneGenerale; 
+        students[index].moyenneGenerale = moyenneGenerale; 
     }
 
     function calculerMoyenneGeneraleClasse() public {
@@ -121,8 +148,9 @@ contract SytemeVoteEleves is Ownable {
         return moyennesParMatiere;
     }*/
 
-    function getMoyenneGenerale(address _adrStudent) public view returns (uint){
-        return students[_adrStudent].moyenneGenerale;
+    function getMoyenneGenerale(string memory _nom, string memory _classe) public view returns (uint){
+        uint index = getEtudiantParNom(_nom, _classe);
+        return students[index].moyenneGenerale;
     }
 
 } 
